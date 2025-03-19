e516df860ca864a132cbed7fa2698c81c0a956ca
Manolis Papadakis <mpapadakis@nvidia.com>
2025-03-19 16:53:16 -0700

    Final example

---
+++ partitioning-dask.py
@@ -0,0 +1,26 @@
+import cupy
+import dask
+from dask.distributed import Client, wait
+from dask_cuda import LocalCUDACluster
+from numpy import complex64
+
+if __name__ == "__main__":
+    cluster = LocalCUDACluster()
+    client = Client(cluster)
+
+def batched_fft(src: cupy.ndarray) -> cupy.ndarray:
+    return cupy.fft.fftn(src, axes=(0,))
+
+if __name__ == "__main__":
+
+    shape = (1024, 1024)
+    # manually make sure we don't split the first dimension
+    chunks = (1024, 512)
+
+    with dask.config.set({"array.backend": "cupy"}):
+        x = dask.array.full(
+            shape, 42, chunks=chunks, dtype=complex64
+        )
+
+    y = x.map_blocks(batched_fft)
+    wait(y.persist())
---
+++ partitioning-legate.py
@@ -0,0 +1,29 @@
+import cupy
+import legate
+from legate.core import align, broadcast, get_legate_runtime
+from legate.core.task import InputStore, OutputStore, task
+from legate.core.types import complex64
+
+GPU = legate.core.VariantCode.GPU
+runtime = get_legate_runtime()
+
+@task(
+    variants=(GPU,),
+    constraints=(
+        # partition these in the same way
+        align("dst", "src"),
+        # don't split the first dimension
+        broadcast("src", (0,))))
+def batched_fft(
+    dst: OutputStore, src: InputStore,
+):
+    cp_dst = cupy.asarray(dst)
+    cp_src = cupy.asarray(src)
+    cp_dst[:] = cupy.fft.fftn(cp_src, axes=(0,))
+
+shape = (1024, 1024)
+x = runtime.create_store(complex64, shape)
+y = runtime.create_store(complex64, shape)
+runtime.issue_fill(y, 42)
+
+batched_fft(x, y)
