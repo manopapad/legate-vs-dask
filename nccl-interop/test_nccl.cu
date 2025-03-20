7ec5b863d88b3e8be9105d42db06a1d84a819ce2
Manolis Papadakis <mpapadakis@nvidia.com>
2025-03-19 17:50:06 -0700

    Fix NCCL example

--- nccl-interop/test_nccl.cu
+++ nccl-interop/test_nccl.cu
@@ -1,27 +1,24 @@
+#include <iostream>
 #include <legate.h>
-
 #include <nccl.h>
 
 namespace lg = legate;
-using legate::Memory::Kind;
 
-struct NCCLTester : public legate::LegateTask<NCCLTester> {
-  static inline const auto TASK_CONFIG = legate::TaskConfig{legate::LocalTaskID{0}};
-  static constexpr auto GPU_VARIANT_OPTIONS =
-    lg::VariantOptions{}
-    .with_concurrent(true)
-    .with_has_allocations(true);
-    .with_communicators({"nccl"});
+constexpr auto ZCMEM = lg::Memory::Kind::Z_COPY_MEM;
 
-  static void gpu_variant(legate::TaskContext context)
+struct NCCLTester : public lg::LegateTask<NCCLTester> {
+
+  static inline const auto TASK_CONFIG = lg::TaskConfig{lg::LocalTaskID{0}};
+  static inline const auto GPU_VARIANT_OPTIONS =
+    lg::VariantOptions{}.with_concurrent(true).with_has_allocations(true).with_communicators({"nccl"});
+
+  static void gpu_variant(lg::TaskContext context)
   {
     auto comm = context.communicators().at(0).get<ncclComm_t*>();
     size_t N = context.get_launch_domain().get_volume();
 
-    auto recv_buf =
-      lg::create_buffer<uint64_t>(N, Kind::Z_COPY_MEM).ptr(0);
-    auto send_buf =
-      lg::create_buffer<uint64_t>(1, Kind::Z_COPY_MEM).ptr(0);
+    auto recv_buf = lg::create_buffer<uint64_t>(N, ZCMEM);
+    auto send_buf = lg::create_buffer<uint64_t>(1, ZCMEM);
 
     auto* p_recv = recv_buf.ptr(0);
     auto* p_send = send_buf.ptr(0);
@@ -37,19 +34,25 @@ struct NCCLTester : public legate::LegateTask<NCCLTester> {
   }
 };
 
-int main() {
-  auto runtime = legate::Runtime::get_runtime();
+int main()
+{
+  lg::start();
+
+  auto runtime = lg::Runtime::get_runtime();
   auto machine = runtime->get_machine();
-  if (machine.count(legate::mapping::TaskTarget::GPU) < 2) {
-    return;
+  if (machine.count(lg::mapping::TaskTarget::GPU) < 2) {
+    std::cerr << "Need at least 2 GPUs" << std::endl;
+    return 1;
   }
 
-  auto library = runtime->create_library("test_nccl", legate::ResourceConfig{},
-                                         nullptr, {});
+  auto library = runtime->create_library("test_nccl", lg::ResourceConfig{}, nullptr, {});
   NCCLTester::register_variants(library);
 
-  auto store = runtime->create_store(legate::full(1, SIZE), legate::int32());
-  auto task = runtime->create_task(context, NCCLTester::TASK_CONFIG.task_id());
+  auto store = runtime->create_store(lg::Shape{10000}, lg::int32());
+  auto task = runtime->create_task(library, NCCLTester::TASK_CONFIG.task_id());
   task.add_output(store);
+  task.add_communicator("nccl");
   runtime->submit(std::move(task));
+
+  std::ignore = lg::finish();
 }
