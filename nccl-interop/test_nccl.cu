0eb4f2b16bb5bf7f7c6e718c4f229a208e4bea5f
Manolis Papadakis <mpapadakis@nvidia.com>
2025-03-19 18:30:27 -0700

    Don't use temporary buffers in NCCL test

--- nccl-interop/test_nccl.cu
+++ nccl-interop/test_nccl.cu
@@ -2,35 +2,60 @@
 #include <legate.h>
 #include <nccl.h>
 
-namespace lg = legate;
+#define CHECK_CUDA(...)                                                        \
+  do {                                                                         \
+    cudaError_t __result__ = (__VA_ARGS__);                                    \
+    check_cuda(__result__, __FILE__, __LINE__);                                \
+  } while (false)
 
-constexpr auto ZCMEM = lg::Memory::Kind::Z_COPY_MEM;
+void check_cuda(cudaError_t error, const char *file, int line) {
+  if (error != cudaSuccess) {
+    fprintf(stderr,
+            "Internal CUDA failure with error %s (%s) in file %s at line %d\n",
+            cudaGetErrorString(error), cudaGetErrorName(error), file, line);
+    exit(error);
+  }
+}
+
+#define CHECK_NCCL(...)                                                        \
+  do {                                                                         \
+    const ncclResult_t result = __VA_ARGS__;                                   \
+    check_nccl(result, __FILE__, __LINE__);                                    \
+  } while (false)
+
+void check_nccl(ncclResult_t error, const char *file, int line) {
+  if (error != ncclSuccess) {
+    static_cast<void>(fprintf(
+        stderr,
+        "Internal NCCL failure with error %d (%s) in file %s at line %d\n",
+        error, ncclGetErrorString(error), file, line));
+    std::exit(error);
+  }
+}
+
+namespace lg = legate;
 
 struct NCCLTester : public lg::LegateTask<NCCLTester> {
 
   static inline const auto TASK_CONFIG = lg::TaskConfig{lg::LocalTaskID{0}};
   static inline const auto GPU_VARIANT_OPTIONS =
-    lg::VariantOptions{}.with_concurrent(true).with_has_allocations(true).with_communicators({"nccl"});
+    lg::VariantOptions{}.with_communicators({"nccl"});
 
   static void gpu_variant(lg::TaskContext context)
   {
+    // Communicators cached by runtime
+    // reused when using the same devices
     auto comm = context.communicators().at(0).get<ncclComm_t*>();
-    size_t N = context.get_launch_domain().get_volume();
 
-    auto recv_buf = lg::create_buffer<uint64_t>(N, ZCMEM);
-    auto send_buf = lg::create_buffer<uint64_t>(1, ZCMEM);
+    auto output = context.output(0);
+    auto shape = output.shape<1>();
+    auto volume = shape.volume();
+    auto ptr = output.data().write_accessor<uint64_t, 1>().ptr(shape.lo);
 
-    auto* p_recv = recv_buf.ptr(0);
-    auto* p_send = send_buf.ptr(0);
-
-    *p_send = 12345;
-
+    // Streams managed by runtime
+    // for tying CUDA effects with task completion
     auto stream = context.get_task_stream();
-    ncclAllGather(p_send, p_recv, 1, ncclUint64, *comm, stream);
-    cudaStreamSynchronize(stream);
-
-    for (std::uint32_t idx = 0; idx < N; ++idx)
-      assert(p_recv[idx] == 12345);
+    CHECK_NCCL(ncclAllReduce(ptr, ptr, volume, ncclUint64, ncclSum, *comm, stream));
   }
 };
 
@@ -38,21 +63,39 @@ int main()
 {
   lg::start();
 
+  constexpr uint64_t size = 100;
   auto runtime = lg::Runtime::get_runtime();
   auto machine = runtime->get_machine();
-  if (machine.count(lg::mapping::TaskTarget::GPU) < 2) {
+  const auto ngpus = machine.count(lg::mapping::TaskTarget::GPU);
+  if (ngpus < 2) {
     std::cerr << "Need at least 2 GPUs" << std::endl;
+    return 1;
+  }
+  if (size % ngpus != 0) {
+    std::cerr << "Number of GPUs must be a multiple of " << size << std::endl;
     return 1;
   }
 
   auto library = runtime->create_library("test_nccl", lg::ResourceConfig{}, nullptr, {});
   NCCLTester::register_variants(library);
 
-  auto store = runtime->create_store(lg::Shape{10000}, lg::int32());
+  auto store = runtime->create_store(lg::Shape{size}, lg::uint64());
+  runtime->issue_fill(store, lg::Scalar{1ul});
+
   auto task = runtime->create_task(library, NCCLTester::TASK_CONFIG.task_id());
+  task.add_input(store);
   task.add_output(store);
-  task.add_communicator("nccl");
   runtime->submit(std::move(task));
 
+  auto p_store = store.get_physical_store();
+  auto acc = p_store.read_accessor<uint64_t, 1>();
+  for (auto it = lg::PointInRectIterator<1>{p_store.shape<1>()}; it.valid(); ++it) {
+    if (acc[*it] != ngpus) {
+      std::cerr << "wrong contents" << std::endl;
+      return 1;
+    }
+  }
+
   std::ignore = lg::finish();
+  return 0;
 }
